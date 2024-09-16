59c55b0ca8ad02f022a26d2a0297d70863d9596f
Manoel Fernando <piloukos45@gmail.com>
2024-09-16 16:28:48 -0300

    teste92

--- src/pages/home/HomeStyles.jsx
+++ src/pages/home/HomeStyles.jsx
@@ -65,7 +65,7 @@ export const Container_home = styled.div`
         display: flex;
         align-items: center;
         justify-content: center;
-        padding: 0px 15px;
+        padding: 0px 20px;
     }
 
     
