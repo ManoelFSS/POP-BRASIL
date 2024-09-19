96a7cef0c856b4172d5f24ed73ad8ccd50584a3a
manoel <piloukos45@gmail.com>
2024-09-19 16:16:22 -0300

    aaa

--- src/context/contextAuth/ContexAuth.jsx
+++ src/context/contextAuth/ContexAuth.jsx
@@ -9,88 +9,124 @@ const provider = new GoogleAuthProvider();
 
 
 export const AuthProvider = ({ children }) => { // exportando a funçao que farar o provider , passando como paramentro children
+ 
+
+  const db_app = getFirestore(app)
+  const userCollectionRef = collection(db_app, "Users")
+
+  const [User, setUser] = useState([])
+  const [auths, setAuth] = useState(JSON.parse(localStorage.getItem("User")))
 
 
-const db_app = getFirestore(app)
-const userCollectionRef = collection(db_app, "Users")
-
-const [User, setUser] = useState([])
-const [auths, setAuth] = useState(JSON.parse(localStorage.getItem("User")))
-
-// const [toogle, setToogle] = useState("-310px")
-
-const getUsers = async () => {
+  const getUsers = async () => {
     const response = await getDocs(userCollectionRef)
     const  result = response.docs.map((doc) => ({...doc.data(), id: doc.id}))
     setUser(result)
-}
+    // if(!auths){
+    //   setReload_Localstorage()
+    // }
+  }
 
-const signInGoogle = () => {
-    getUsers();
+ 
+  const signInGoogle = () => {
+    getUsers()
     const auth = getAuth(app);
 
-    signInWithPopup(auth, provider)
+    if(User === null){
+        
+      auth.signInWithRedirect(provider)
         .then((result) => {
             const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
             const user = result.user;
-
-            let checkedUser = false;
-
-            User.forEach((e) => {
-                if (e.email === user.providerData[0].email && e.token === user.providerData[0].uid) {
-                    setAuth(e.adm);
-                    localStorage.setItem("User", JSON.stringify(e.adm));
-                    localStorage.setItem("photo", JSON.stringify(user.providerData[0].photoURL));
-                    localStorage.setItem("UserName", JSON.stringify(user.providerData[0].displayName));
-                    checkedUser = true;
-                }
-            });
-
-            if (!checkedUser) {
-                createUser(user.providerData[0]);
-                setAuth(false);
-                localStorage.setItem("User", JSON.stringify(false));
-                localStorage.setItem("photo", JSON.stringify(user.providerData[0].photoURL));
-                localStorage.setItem("UserName", JSON.stringify(user.providerData[0].displayName));
-            }
-
-            getUsers();
-        })
-        .catch((error) => {
-            // Trate erros aqui
+            createUser(user.providerData[0])
+            getUsers()
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            const email = error.customData.email;
+            const credential = GoogleAuthProvider.credentialFromError(error);
         });
-};
 
-async function createUser(user){
+    }else{
+      
+      console.log(User)
+      signInWithPopup(auth, provider)
+      .then((result) => {
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          const token = credential.accessToken;
+          const user = result.user;
+
+          let checkedUser = false
+          if(User){
+            setAuth(null)
+            getUsers()
+            User.map((e)=> {
+              if(e.email === user.providerData[0].email && e.token === user.providerData[0].uid){
+                console.log("usuario ja exite")
+                setAuth(e.adm)
+                localStorage.setItem("User", JSON.stringify(e.adm))
+                localStorage.setItem("photo", JSON.stringify(user.providerData[0].photoURL))
+                localStorage.setItem("UserName", JSON.stringify(user.providerData[0].displayName))
+                console.log(User[0].adm)
+                checkedUser = true
+              }
+            })
+          }
+          if(!checkedUser){
+            createUser(user.providerData[0])
+            console.log("new usuario")
+            setAuth(false)
+            localStorage.setItem("User", JSON.stringify(false))
+            localStorage.setItem("photo", JSON.stringify(user.providerData[0].photoURL))
+            localStorage.setItem("UserName", JSON.stringify(user.providerData[0].displayName))
+            checkedUser = false
+         
+          }
+          
+          getUsers()
+      }).catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          const email = error.customData.email;
+          const credential = GoogleAuthProvider.credentialFromError(error);
+      });
+    }
+  }
+  
+  async function createUser(user){
     
     await addDoc(userCollectionRef, {
-        displayName:user.displayName,
-        email:user.email,
-        photo:user.photoURL,
-        token:user.uid,
-        adm: User === null ? true : false,
+      displayName:user.displayName,
+      email:user.email,
+      photo:user.photoURL,
+      token:user.uid,
+      adm: User === null ? true : false,
     })
     setAuth(User === null ? true : false)
     localStorage.setItem("User", JSON.stringify(User === null ? true : false))
     getUsers()
-}
+  }
 
-useEffect(()=>{
-    getUsers()
-},[])
+
+    useEffect(()=>{
+       getUsers()
+    },[])
+
+
 
     return ( // usando e retornando o Appcontext.provider e passando o children, passando valores como props  pelo value 
         <AuthContext.Provider value={{ 
+
             signInGoogle,
-            auths
+
         }}>
             {children}
         </AuthContext.Provider>
     );
-
 };
 
 export const useAuthContext = () => {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 };
 
