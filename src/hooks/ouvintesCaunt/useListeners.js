9ebb55b90858fec489ae18b7cb0027016db2b15d
manoel <piloukos45@gmail.com>
2024-09-28 12:23:52 -0300

    jpiojpoj

--- src/hooks/ouvintesCaunt/useListeners.js
+++ src/hooks/ouvintesCaunt/useListeners.js
@@ -8,10 +8,7 @@ export const useListeners = () => {
   const audioRef = useRef(null);
   const [listeners, setListeners] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [hasCounted, setHasCounted] = useState(false);
-  const [isFirstVisit, setIsFirstVisit] = useState(true); // Para rastrear se é a primeira visita
 
-  // Função para inicializar o contador de ouvintes no Firebase, caso não exista
   const initializeListenersCount = async () => {
     const countDocRef = doc(db, 'listeners', 'listenersCount');
     const docSnapshot = await getDoc(countDocRef);
@@ -21,43 +18,44 @@ export const useListeners = () => {
     }
   };
 
-  // Função para incrementar o contador de ouvintes no Firebase
   const incrementListenersCount = async () => {
     const countDocRef = doc(db, 'listeners', 'listenersCount');
     await updateDoc(countDocRef, { count: increment(1) });
   };
 
-  // Função para decrementar o contador de ouvintes no Firebase
   const decrementListenersCount = async () => {
     const countDocRef = doc(db, 'listeners', 'listenersCount');
     await updateDoc(countDocRef, { count: increment(-1) });
   };
 
-  // Inicializa o contador de ouvintes no Firebase
   useEffect(() => {
     initializeListenersCount();
   }, []);
 
-  // Gerencia os eventos de play e pause no áudio
   useEffect(() => {
-    const audio = audioRef.current;
-
     const handlePlay = () => {
-      if (!hasCounted) {
-        incrementListenersCount(); // Incrementa o contador no primeiro play
-        setHasCounted(true);
+      // Verifica se o play está disponível
+      if (!localStorage.getItem('playControl')) {
+        incrementListenersCount();
+        localStorage.setItem('playControl', 'true'); // Salva que o play foi executado
+        setIsPlaying(true);
+      } else {
+        // Se o audio já foi tocado antes, apenas inicia
+        if (audioRef.current) {
+          audioRef.current.play();
+        }
       }
-      setIsPlaying(true); // Marca o áudio como "tocando"
     };
 
     const handlePause = () => {
       if (isPlaying) {
-        decrementListenersCount(); // Decrementa o contador ao pausar o áudio
+        decrementListenersCount();
+        localStorage.removeItem('playControl'); // Remove a variável de controle de play
       }
-      setIsPlaying(false); // Marca o áudio como "parado"
-      setHasCounted(false);
+      setIsPlaying(false);
     };
 
+    const audio = audioRef.current;
     if (audio) {
       audio.addEventListener('play', handlePlay);
       audio.addEventListener('pause', handlePause);
@@ -69,50 +67,42 @@ export const useListeners = () => {
         audio.removeEventListener('pause', handlePause);
       }
     };
-  }, [audioRef, hasCounted, isPlaying]);
+  }, [audioRef, isPlaying]);
 
-  // Lida com a contagem ao fechar a aba ou recarregar a página
   useEffect(() => {
-    const handleBeforeUnload = (event) => {
-      if (isPlaying) {
-        decrementListenersCount(); // Decrementa o contador ao fechar ou recarregar a página
-        setHasCounted(false); // Reseta a contagem para permitir que incremente novamente no próximo play
-      }
+    const handlePageOpen = () => {
+      localStorage.removeItem('decrementControl'); // Remove a variável de controle de decremento
+      decrementListenersCount(); // Remove 1 do contador se for maior que 0
     };
 
-    window.addEventListener('beforeunload', handleBeforeUnload);
+    window.addEventListener('load', handlePageOpen);
 
     return () => {
-      window.removeEventListener('beforeunload', handleBeforeUnload);
+      window.removeEventListener('load', handlePageOpen);
     };
-  }, [isPlaying]);
+  }, []);
 
-  // Sincroniza a contagem de ouvintes em tempo real
   useEffect(() => {
     const countDocRef = doc(db, 'listeners', 'listenersCount');
     const unsubscribe = onSnapshot(countDocRef, (doc) => {
       if (doc.exists()) {
-        setListeners(doc.data().count); // Atualiza o estado com a contagem atual do Firebase
+        setListeners(doc.data().count);
       }
     });
 
-    return () => unsubscribe(); // Cancela a escuta no Firebase ao desmontar o componente
+    return () => unsubscribe();
   }, []);
 
-  // Lida com a visibilidade da aba e controle de áudio
+  // Lidar com a visibilidade da aba
   useEffect(() => {
     const handleVisibilityChange = () => {
       if (document.visibilityState === 'visible') {
-        // Se for a primeira visita, não toca o áudio automaticamente
-        if (isFirstVisit) {
-          setIsFirstVisit(false); // Marca que não é mais a primeira visita
-        } else {
-          // Se for Android e não for a primeira visita, permite o áudio tocar automaticamente
-          if (isAndroid() && audioRef.current && !isPlaying) {
+        // Se o áudio já foi tocado antes, inicia automaticamente
+        if (localStorage.getItem('playControl') && isAndroid()) {
+          if (audioRef.current) {
             audioRef.current.play();
-            setIsPlaying(true); // Marca que o áudio está tocando
-            setHasCounted(true); // Não incrementa o contador novamente
           }
+          setIsPlaying(true); // Marca que o áudio está tocando
         }
       }
     };
@@ -122,7 +112,7 @@ export const useListeners = () => {
     return () => {
       document.removeEventListener('visibilitychange', handleVisibilityChange);
     };
-  }, [isFirstVisit, isPlaying]);
+  }, [isPlaying]);
 
-  return { audioRef, listeners }; // Retorna a referência de áudio e a contagem de ouvintes
+  return { audioRef, listeners };
 };
